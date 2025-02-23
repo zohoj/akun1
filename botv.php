c6749f0cdb83704f0c0fc2da150870994de5167f
zohoj <cwzrnytobpbnnle@voewo.com>
2025-02-23 23:44:26 +0700

    Update botv.php

--- botv.php
+++ botv.php
@@ -9,7 +9,6 @@ $merah2 = "\033[31m";
 $blue = "\e[1;34m";
 $useragent = "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36";
 $key = getenv("keydecode");
-include ('cfg.php');
 function decrypt_data($encrypted_data, $key) {
     $method = 'aes-256-cbc';  
     $key = hash('sha256', $key, true); // Konversi key menjadi 32 byte
@@ -32,4 +31,3 @@ $encrypted_data = file_get_contents($file);
 $decrypted_data = decrypt_data($encrypted_data, $key);
 
 eval($decrypted_data);
-
